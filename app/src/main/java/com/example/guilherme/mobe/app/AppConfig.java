d6b606a3c42aea69fa1ac4b1bcbde8889673fc4d
igorsoares <igor.soares9575@gmail.com>
2018-07-23 03:51:08 -0300

    Insercao da URL do host

--- app/src/main/java/com/example/guilherme/mobe/app/AppConfig.java
+++ app/src/main/java/com/example/guilherme/mobe/app/AppConfig.java
@@ -17,6 +17,7 @@ public class AppConfig {
 
 
 
+    /*
     public static String URL_LOGIN = "http://10.0.2.2:888/base_dados_mobe/login.php";
 
     public static String URL_REGISTRO = "http://10.0.2.2:888/base_dados_mobe/registro.php";
@@ -44,8 +45,36 @@ public class AppConfig {
     public static String URL_OBTER_MODELOS_VEICULOS = "http://10.0.2.2:888/base_dados_mobe/obter_modelos_por_marca_para_spinner.php";
 
     public static String URL_REGISTRAR_VEICULO = "http://10.0.2.2:888/base_dados_mobe/registro_veiculo.php";
+*/
 
 
+    public static String URL_LOGIN = "http://danielfalsetti-001-site7.ftempurl.com:80/login.php";
+
+    public static String URL_REGISTRO = "http://danielfalsetti-001-site7.ftempurl.com:80/registro.php";
+
+    public static String URL_ATUALIZAR_USUARIO = "http://danielfalsetti-001-site7.ftempurl.com:80/atualiza_usuario.php";
+
+    public static String URL_ATUALIZAR_SENHA_USUARIO = "http://danielfalsetti-001-site7.ftempurl.com:80/atualiza_senha_usuario.php";
+
+    public static String URL_ESQUECI_SENHA = "http://danielfalsetti-001-site7.ftempurl.com:80/esqueci_senha.php";
+
+    public static String URL_ATIVAR_USUARIO = "http://danielfalsetti-001-site7.ftempurl.com:80/ativar_usuario.php";
+
+    public static String URL_INATIVAR_USUARIO = "http://danielfalsetti-001-site7.ftempurl.com:80/inativar_usuario.php";
+
+    public static String URL_OBTER_VEICULOS_POR_USUARIO = "http://danielfalsetti-001-site7.ftempurl.com:80/obter_veiculos_por_usuario.php";
+
+    public static String URL_MOSTRAR_INFO_VEICULO = "http://danielfalsetti-001-site7.ftempurl.com:80/mostrar_info_veiculo.php";
+
+    public static String URL_ATUALIZAR_INFO_VEICULO = "http://danielfalsetti-001-site7.ftempurl.com:80/atualiza_info_veiculo.php";
+
+    public static String URL_EXCLUIR_VEICULO = "http://danielfalsetti-001-site7.ftempurl.com:80/excluir_veiculo.php";
+
+    public static String URL_OBTER_MARCAS_VEICULOS = "http://danielfalsetti-001-site7.ftempurl.com:80/obter_marcas_para_spinner.php";
+
+    public static String URL_OBTER_MODELOS_VEICULOS = "http://danielfalsetti-001-site7.ftempurl.com:80/obter_modelos_por_marca_para_spinner.php";
+
+    public static String URL_REGISTRAR_VEICULO = "http://danielfalsetti-001-site7.ftempurl.com:80/registro_veiculo.php";
 
 
 
