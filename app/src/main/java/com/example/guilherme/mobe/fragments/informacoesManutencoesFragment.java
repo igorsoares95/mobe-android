338cadea2ff5810f339ba0d6649b35b2eeea9cff
GUILHERME <guilhermerodrigues73@gmail.com>
2018-08-04 13:42:15 -0300

    Ajustes nas fragments, criação de Fragment de Informações das Manutenções e Manutenções Padrão

---
+++ app/src/main/java/com/example/guilherme/mobe/fragments/informacoesManutencoesFragment.java
@@ -0,0 +1,30 @@
+package com.example.guilherme.mobe.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.guilherme.mobe.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class informacoesManutencoesFragment extends Fragment {
+
+
+    public informacoesManutencoesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_informacoes_manutencoes, container, false);
+    }
+
+}
